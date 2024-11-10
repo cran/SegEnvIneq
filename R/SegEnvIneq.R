105a0a5c1df53281fbbb0c45592b071266ec2c8c
Mihai Tivadar <mihai.tivadar@inrae.fr>
2024-11-10 15:50:06 +0000

    version 1.2

--- R/SegEnvIneq.R
+++ R/SegEnvIneq.R
@@ -9,43 +9,43 @@ library(sf)
 
 #' A function to compute environmental dissimilarity index
 #'
-#' @usage EDfunc (x, a, vers = "standard", w = NULL, ar = NULL, per = NULL,
+#' @usage EDfunc(x, a, vers = "standard", w = NULL, ar = NULL, per = NULL,
 #' b = NULL, folder = NULL, shape = NULL, spatobj = NULL, queen = TRUE,
 #' ptype = "int", K = 1, f = "exp", beta = 1)
-#' @param x - a vector of the population/group distribution across spatial units
-#' @param a - a vector of the environmental variable spatial distribution
-#' @param vers - the index version:
+#' @param x a vector of the population/group distribution across spatial units
+#' @param a a vector of the environmental variable spatial distribution
+#' @param vers the index version:
 #' "standard" (by default) for aspatial environmental dissimilarity index (Duncan);
 #' "contig" for adjusted index with a contiguity spatial interactions matrix (Morrill);
 #' "bound" for adjusted index with a boundaries spatial interactions matrix (Wong);
 #' "shape" for adjusted index with a boundaries and shape spatial interactions matrix (Wong);
 #' "user" for adjusted index with any user spatial interactions matrix
-#' @param w - an optional spatial weights matrix.
+#' @param w an optional spatial weights matrix.
 #' If necessary and not provided, it will be computed in the function
-#' @param ar - an optional vector of spatial units area.
+#' @param ar an optional vector of spatial units area.
 #' If necessary and not provided, it will be computed in the function
-#' @param b - an optional shared border matrix.
+#' @param b an optional shared border matrix.
 #' If necessary and not provided, it will be computed in the function.
-#' @param per - an optional vector of spatial units perimeter.
+#' @param per an optional vector of spatial units perimeter.
 #' If necessary and not provided, it will be computed in the function.
-#' @param folder - a character vector with the folder (directory) name
+#' @param folder a character vector with the folder (directory) name
 #' indicating where the shapefile with geographical info is located on the drive
 #' if the interactions matrix is computed in the function
-#' @param shape - a character vector with the name of the shapefile
+#' @param shape a character vector with the name of the shapefile
 #' (without the .shp extension)
-#' @param spatobj - a spatial object (SpatialPolygonsDataFrame) with
+#' @param spatobj a spatial object (SpatialPolygonsDataFrame) with
 #' geographic information as alternative for the shapefile,
 #' if the interactions matrix is computed in the function
-#' @param queen - logical parameter defining criteria used for contiguity matrix computation,
+#' @param queen logical parameter defining criteria used for contiguity matrix computation,
 #' TRUE for queen (by default), FALSE  for rook
-#' @param ptype - a string variable giving two options for perimeter calculation for  Wong's indices:
+#' @param ptype a string variable giving two options for perimeter calculation for  Wong's indices:
 #' "int" to use only interior borders of spatial units and
 #' "all" to use entire borders, including to the exterior of the area
-#' @param K - the order of contiguity matrix if "contig" version is chosen (K = 1 by default)
-#' @param f - spatial decay function of contiguity matrix when K > 1, with
+#' @param K the order of contiguity matrix if "contig" version is chosen (K = 1 by default)
+#' @param f spatial decay function of contiguity matrix when K > 1, with
 #' f = "exp" (by default) for exponential function of contiguity "distance" exp(beta*(1-k))
 #  and f = "rec" for 1/(k^beta)
-#' @param beta - spatial decay intensity parameter (equal to 1 by default),
+#' @param beta spatial decay intensity parameter (equal to 1 by default),
 #' used only when the version with contiguity is chosen and K > 1
 #' @return The value of the environmental dissimilarity index
 #' @references Schaeffer Y. and Tivadar M. (2019)
@@ -189,31 +189,31 @@ EDfunc <- function (x, a, vers = "standard", w = NULL, ar = NULL, per = NULL,
 
 #' A function to compute environmental centralization index
 #'
-#' @usage ECfunc (x, distmin = NULL, dist = NULL, K = NULL, kdist = NULL,
+#' @usage ECfunc(x, distmin = NULL, dist = NULL, K = NULL, kdist = NULL,
 #' spatobj1 = NULL, folder1 = NULL, shape1 = NULL,
 #' spatobj2 = NULL, folder2 = NULL, shape2 = NULL)
-#' @param x - a matrix with the groups distributions across spatial units
-#' @param distmin - an optional vector with the minimal distance between each spatial
+#' @param x a matrix with the groups distributions across spatial units
+#' @param distmin an optional vector with the minimal distance between each spatial
 #' unit and all the environmental localisations. If not provided, it will be computed
 #' in the function
-#' @param dist - an optional matrix with the distance between all spatial units
+#' @param dist an optional matrix with the distance between all spatial units
 #' and environmental localisations. If not provided, it will be computed in the function
-#' @param K - if provided, the version of the index constrained to the K nearest neighbors
-#' @param kdist - if provided, the version of the index constrained to  the nearest neighbors
+#' @param K if provided, the version of the index constrained to the K nearest neighbors
+#' @param kdist if provided, the version of the index constrained to  the nearest neighbors
 #' within a distance of kdist
-#' @param spatobj1 - polygons spatial objects for population distribution to compute distances matrix
+#' @param spatobj1 polygons spatial objects for population distribution to compute distances matrix
 #' (necessary if distance not provided).
-#' @param folder1 - a character vector with the folder (directory) name
+#' @param folder1 a character vector with the folder (directory) name
 #' indicating where the shapefile with geographical info of population distribution is located
 #' on the drive (necessary if distance and spatial object are not provided).
-#' @param shape1 - a character vector with the name of the shapefile
+#' @param shape1 a character vector with the name of the shapefile
 #' (without the .shp extension) with geographical info of population distribution
-#' @param spatobj2 - points spatial objects for (dis-)amenity location to compute distances matrix
+#' @param spatobj2 points spatial objects for (dis-)amenity location to compute distances matrix
 #' (necessary if distance not provided).
-#' @param folder2 - a character vector with the folder (directory) name
+#' @param folder2 a character vector with the folder (directory) name
 #' indicating where the shapefile with geographical info of (dis-)amenity distribution is located
 #' on the drive (necessary if distance and spatial object are not provided).
-#' @param shape2 - a character vector with the name of the shapefile
+#' @param shape2 a character vector with the name of the shapefile
 #' (without the .shp extension) with geographical info of (dis-)amenity spatial location
 #' @return The matrix with environmental centralization index values
 #' @references Schaeffer Y. and Tivadar M. (2019)
@@ -316,76 +316,75 @@ ECfunc <- function (x, distmin = NULL, dist = NULL, K = NULL, kdist = NULL,
 #' A function to test environmental inequality indices by resampling
 #'
 #' @usage EnvResampleTest(x, a = NULL, fun, simtype = "MonteCarlo",
-#' nsim = NULL, sampleunit = "unit", proba = NULL, setseed = FALSE,
-#' perc = c(.05, .95), outl = FALSE, outmeth = "bp", sdtimes = 2, IQRrange = 1.5,
+#' nsim = NULL, sampleunit = "unit", proba = NULL, perc = c(.05, .95),
+#' outl = FALSE, outmeth = "bp", sdtimes = 2, IQRrange = 1.5,
 #' spatobj = NULL, folder = NULL, shape = NULL,
 #' spatobj1 = NULL, folder1 = NULL, shape1 = NULL,
 #' spatobj2 = NULL, folder2 = NULL, shape2 = NULL,
 #' distmin = NULL, dist = NULL, K = NULL, kdist = NULL,
 #' vers = "standard", w = NULL, b = NULL, ar = NULL, per = NULL,
 #' queen = TRUE, ptype = "int", f = "exp", beta = 1)
-#' @param x -  a vector of the population/group distribution across spatial units for EDfunc or
+#' @param x  a vector of the population/group distribution across spatial units for EDfunc or
 #' a matrix with the groups distributions across spatial units for ECfunc
-#' @param a - a vector of the environmental variable spatial distribution for EDfunc
-#' @param fun - a character vector with the function to be tested, fun = "EDfunc" or fun = "ECfunc"
-#' @param simtype - a character vector with the type of simulation.
+#' @param a a vector of the environmental variable spatial distribution for EDfunc
+#' @param fun a character vector with the function to be tested, fun = "EDfunc" or fun = "ECfunc"
+#' @param simtype a character vector with the type of simulation.
 #' If simtype = 'MonteCarlo' (by default), the function produces a randomization test
 #' using Monte Carlo simulations. If simtype = 'Jack', the function generates jackknife replications
-#' @param nsim	- the number of simulations (equal to the number of observations for jackknife)
-#' @param sampleunit - for jackknife replicant, the resampling is made only on spatial units.
+#' @param nsim the number of simulations (equal to the number of observations for jackknife)
+#' @param sampleunit for jackknife replicant, the resampling is made only on spatial units.
 #' For Monte Carlo simulations, the user can choose between "unit" for spatial units
 #' resampling and "ind" for population resampling.
-#' @param proba - for Monte Carlo simulations on population, proba is a vector with location probabilities.
+#' @param proba for Monte Carlo simulations on population, proba is a vector with location probabilities.
 #' If proba = NULL, the vector is equiprobable. If outliers are determined with jackknife technique,
 #' proba indicates the probability (confidence interval) for scoring tests.
-#' @param setseed - if TRUE (by default), specify zero seed for repetead simulation
-#' @param perc - percentille values for jackknife simulations
-#' @param outl	- logical parameter for jackknife simulations, if TRUE the function
+#' @param perc percentille values for jackknife simulations
+#' @param outl logical parameter for jackknife simulations, if TRUE the function
 #' provides the outliers obtained by jackknife iterations
-#' @param outmeth - a character vector designing the outliers detection method:
+#' @param outmeth a character vector designing the outliers detection method:
 #' outmeth = 'bp' (by default) for boxplot method; outmeth = 'sd' for standard deviation method;
 #' outmeth = 'z' for normal scores method; outmeth = 't' for t Student scores method;
 #' outmeth = 'chisq' for chi-squared scores method; outmeth = 'mad' for median absolute deviation
 #' method. The estimations based on scoring methods are obtained using outliers package
-#' @param sdtimes - multiplication factor of the standard deviation used for
+#' @param sdtimes multiplication factor of the standard deviation used for
 #' outliers detection with jackknife simulations (2 by default)
-#' @param IQRrange	- determines the boxplot thresholds (1.5 by default) as multiplication of IQR (Inter Quartile Range)
-#' @param spatobj - a spatial object (SpatialPolygonsDataFrame) for EDfunc geographic functions
-#' @param shape - a character vector with the name of the shapefile for EDfunc as alternative to spatobj
-#' @param folder - a character vector with the folder (directory) containing the shapefile for EDfunc
-#' @param spatobj1 - polygons spatial objects for population distribution to compute distances matrix for ECfunc
-#' @param shape1 - a character vector with the name of the shapefile for ECfunc as alternative to spatobj1
-#' @param folder1 - a character vector with the folder (directory) containing the shape1 for ECfunc
-#' @param spatobj2 - points spatial objects for (dis-)amenity location to compute distances matrix for ECfunc
-#' @param shape2 - a character vector with the name of the shapefile for ECfunc as alternative to spatobj2
-#' @param folder2 - a character vector with the folder (directory) containing the shape2 for ECfunc
-#' @param distmin - an optional vector for ECfunc with the minimal distance between each spatial
+#' @param IQRrange determines the boxplot thresholds (1.5 by default) as multiplication of IQR (Inter Quartile Range)
+#' @param spatobj a spatial object (SpatialPolygonsDataFrame) for EDfunc geographic functions
+#' @param shape a character vector with the name of the shapefile for EDfunc as alternative to spatobj
+#' @param folder a character vector with the folder (directory) containing the shapefile for EDfunc
+#' @param spatobj1 polygons spatial objects for population distribution to compute distances matrix for ECfunc
+#' @param shape1 a character vector with the name of the shapefile for ECfunc as alternative to spatobj1
+#' @param folder1 a character vector with the folder (directory) containing the shape1 for ECfunc
+#' @param spatobj2 points spatial objects for (dis-)amenity location to compute distances matrix for ECfunc
+#' @param shape2 a character vector with the name of the shapefile for ECfunc as alternative to spatobj2
+#' @param folder2 a character vector with the folder (directory) containing the shape2 for ECfunc
+#' @param distmin an optional vector for ECfunc with the minimal distance between each spatial
 #' unit and all the environmental localisations
-#' @param dist - an optional matrix for ECfunc with the distance between all spatial units
+#' @param dist an optional matrix for ECfunc with the distance between all spatial units
 #' and environmental localisations.
-#' @param K - if provided, the version of the index constrained to the K nearest neighbors
-#' @param kdist - if provided, the version of the index constrained to  the nearest neighbors
+#' @param K if provided, the version of the index constrained to the K nearest neighbors
+#' @param kdist if provided, the version of the index constrained to  the nearest neighbors
 #' within a distance of kdist
-#' @param vers - the EDfunc version:
+#' @param vers the EDfunc version:
 #' "standard" (by default) for aspatial environmental dissimilarity index (Duncan);
 #' "contig" for adjusted index with a contiguity spatial interactions matrix (Morrill);
 #' "bound" for adjusted index with a boundaries spatial interactions matrix (Wong);
 #' "shape" for adjusted index with a boundaries and shape spatial interactions matrix (Wong);
 #' "user" for adjusted index with any user spatial interactions matrix
-#' @param w - an optional spatial weights matrix for EDfunc.
-#' @param ar - an optional vector of spatial units area for EDfunc.
-#' @param b - an optional shared border matrix for EDfunc.
-#' @param per - an optional vector of spatial units perimeter for EDfunc.
-#' @param queen - logical parameter for EDfunc defining criteria used for contiguity matrix computation,
+#' @param w an optional spatial weights matrix for EDfunc.
+#' @param ar an optional vector of spatial units area for EDfunc.
+#' @param b an optional shared border matrix for EDfunc.
+#' @param per an optional vector of spatial units perimeter for EDfunc.
+#' @param queen logical parameter for EDfunc defining criteria used for contiguity matrix computation,
 #' TRUE for queen (by default), FALSE  for rook
-#' @param ptype - a string variable for EDfunc giving two options for perimeter calculation for  Wong's indices:
+#' @param ptype a string variable for EDfunc giving two options for perimeter calculation for  Wong's indices:
 #' "int" to use only interior borders of spatial units and
 #' "all" to use entire borders, including to the exterior of the area
-#' @param K - the order of contiguity matrix if "contig" version of EDfunc is chosen (K = 1 by default)
-#' @param f - spatial decay function of contiguity matrix for EDfunc when K > 1, with
+#' @param K the order of contiguity matrix if "contig" version of EDfunc is chosen (K = 1 by default)
+#' @param f spatial decay function of contiguity matrix for EDfunc when K > 1, with
 #' f = "exp" (by default) for exponential function of contiguity "distance"
 #  and f = "rec" for 1/(k^beta)
-#' @param beta - spatial decay intensity parameter for EDfunc (equal to 1 by default),
+#' @param beta spatial decay intensity parameter for EDfunc (equal to 1 by default),
 #' used only when the version with contiguity is chosen and K > 1
 #' @return A list with:
 #' - index's name
@@ -417,7 +416,7 @@ ECfunc <- function (x, distmin = NULL, dist = NULL, K = NULL, kdist = NULL,
 
 
 EnvResampleTest <- function(x, a = NULL, fun, simtype = "MonteCarlo", nsim = NULL, sampleunit = "unit", proba = NULL,
-                            setseed = FALSE, perc = c(.05, .95), outl = FALSE, outmeth = "bp", sdtimes = 2, IQRrange = 1.5,
+                            perc = c(.05, .95), outl = FALSE, outmeth = "bp", sdtimes = 2, IQRrange = 1.5,
                             spatobj = NULL, folder = NULL, shape = NULL,
                             spatobj1 = NULL, folder1 = NULL, shape1 = NULL,
                             spatobj2 = NULL, folder2 = NULL, shape2 = NULL,
@@ -426,7 +425,7 @@ EnvResampleTest <- function(x, a = NULL, fun, simtype = "MonteCarlo", nsim = NUL
                             queen = TRUE, ptype = "int", f = "exp", beta = 1) {
 
   # INIT
-  if (setseed) set.seed(0)
+
   x <- as.matrix(x)
   if (simtype == "Jack" & is.null(nsim)) nsim <- nrow(x)  else if (is.null(nsim)) nsim <-99
   ntot <- nsim + 1
@@ -637,18 +636,18 @@ EnvResampleTest <- function(x, a = NULL, fun, simtype = "MonteCarlo", nsim = NUL
 #'
 #' @usage EnvResamplePlot(ResampleTest, var = 1,  coldist = "red", colind = "blue", legend = TRUE,
 #' legendpos = "top", cex.legend = 1, bty = "o")
-#' @param ResampleTest - a ResampleTest object produced with ResampleTest function
-#' @param var - the number of the variable to be plot
-#' @param coldist - color used to plot the simulated distribution
-#' @param colind	- color used to plot the index
-#' @param legend	- logical parameter, to control the legend's plots
-#' @param legendpos - a character string giving the legend's position: 'bottomright', 'bottom',
+#' @param ResampleTest a ResampleTest object produced with ResampleTest function
+#' @param var the number of the variable to be plot
+#' @param coldist color used to plot the simulated distribution
+#' @param colind	color used to plot the index
+#' @param legend	logical parameter, to control the legend's plots
+#' @param legendpos a character string giving the legend's position: 'bottomright', 'bottom',
 #' 'bottomleft', 'left', 'topleft', 'top', 'topright', 'right' and 'center'.
-#' @param cex.legend - a numerical value giving the amount by which plotting text and symbols
+#' @param cex.legend a numerical value giving the amount by which plotting text and symbols
 #' in legend should be magnified relative to the default.
-#' @param bty - a character string which determines the type of box of the legend:
+#' @param bty a character string which determines the type of box of the legend:
 #' 'o' (by default), 'l', '7', 'c', 'u', or ']'
-#' @return A plot with resampling distribution
+#' @return A plot with resampling theoretical distribution
 #' corresponding upper case letter. A value of 'n' suppresses the box.
 #' @references Schaeffer Y. and Tivadar M. (2019)
 #' Measuring Environmental Inequalities: Insights from the Residential
